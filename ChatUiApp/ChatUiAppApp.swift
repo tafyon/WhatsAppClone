4786ee8bbfaa4c3c8aeed97dbd5b0d4b9c63181b
Tamer Afyon <203502050@ogr.altinbas.edu.tr>
2024-04-12 19:18:05 +0300

    Initial Commit

---
+++ ChatUiApp/ChatUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUiAppApp.swift
+//  ChatUiApp
+//
+//  Created by Tamer Afyon on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
